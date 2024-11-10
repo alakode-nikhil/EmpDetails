95291e3cae8c1bc6efb40503b59496705d4d3e68
alakode-nikhil <alakodenikhil@gmail.com>
2024-11-10 19:43:25 +0530

    add employee model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    job = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    img = models.ImageField(upload_to='profiles/')
