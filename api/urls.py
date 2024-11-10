2ab25d4449996b55724025e70d7b3dac1717498e
alakode-nikhil <alakodenikhil@gmail.com>
2024-11-10 21:00:20 +0530

    add views and urls

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,9 @@
-
+from django.urls import path
+from .views import *
 urlpatterns = [
-    
+    path('create-employee/', CreateEmployeeApi.as_view(), name='create-employee'),
+    path('retrieve-employee/<int:pk>/', RetrieveEmployeeApi.as_view(), name='retrieve-employee'),
+    path('update-employee/<int:pk>', UpdateEmployeeApi.as_view(),name='update-employee'),
+    path('search-employee/<str:employee_name>/', SearchEmployeeApi.as_view(), name='search-employee'),
+    path('delete-recipe/<int:pk>/', DeleteEmployeeApi.as_view(),name='delete-employee'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import generics,permissions
+from .models import *
+from .serializers import *
 
 # Create your views here.
+class CreateEmployeeApi(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [permissions.AllowAny]
+
+class RetrieveEmployeeApi(generics.RetrieveAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class UpdateEmployeeApi(generics.RetrieveUpdateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class SearchEmployeeApi(generics.ListAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+    def get_queryset(self):
+        employee_name = self.kwargs.get('employee')
+        return Employee.objects.filter(name__icontains = employee_name)
+    
+class DeleteEmployeeApi(generics.DestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
